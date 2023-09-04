5532c826f6deeed81b258b13e4f70fe17cc6d28b
Виктор Вертегел <vertegel@gmail.com>
2023-09-04 10:35:56 +0300

    Add controllers

--- server/index.js
+++ server/index.js
@@ -3,12 +3,14 @@ const express = require('express');
 const sequelize = require('./db');
 const models = require('./models/models')
 const cors = require('cors')
+const router = require('./routes/index')
 
 const PORT = process.env.PORT || 5000;
 
 const app = express();
 app.use(cors())
 app.use(express.json())
+app.use('/api', router)
 
 app.get('/', (req, res) => {
     res.status(200).json({message: "РОБОТОЕТ !!!!"})
@@ -21,8 +23,10 @@ const start = async () => {
 
         app.listen(PORT, () => console.log(`Server startedd on port ${PORT}`))
     }
-    catch {
-
+    catch (e) {
+        console.log("Хуйня")
+        console.log(e)
+        console.log(process.env.DB_NAME)
     }
 }
 
